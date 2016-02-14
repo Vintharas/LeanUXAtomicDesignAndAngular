6424a84d2005e48d5f3a47af9734098f32109fbf
Jaime González García <jaime.gg83@gmail.com>
2016-02-14 17:21:27 +0100

    Started creating a monolithic version of a Pomodoro App
        - created a project using the angular-gulp-generator
        - removed some of the stuff :)
        - started developing the pomodoro app in main.js
            - basic pomodoro timer functionality (start, cancel)
            - list and add new tasks
        - super basic styles

---
+++ 001-monolithic/src/app/main/main.controller.js
@@ -0,0 +1,73 @@
+export class MainController {
+  constructor ($interval, $log) {
+    'ngInject';
+    const vm = this,
+          currentTime = getPomorodoTime();
+
+    Object.assign(vm, {
+        // timer
+        timeLeft: formatTime(currentTime),
+        currentTime,
+        // tasks
+        tasks: getInitialTasks(),
+        newTask: getNewTask(),
+        $interval,
+        $log
+    });
+  }
+
+  startPomodoro(){
+      this.performingTask = true;
+      this.timerInterval = this.$interval(this.advanceTimer.bind(this), 1000);
+  }
+  cancelPomodoro(){
+      this.performingTask = false;
+      this.$interval.cancel(this.timerInterval);
+      this.resetPomodoroTimer();
+  }
+  advanceTimer(){
+      this.$log.debug('advancing timer 1 second');
+      this.currentTime -= 1;
+      this.timeLeft = formatTime(this.currentTime);
+  }
+  addNewTask(){
+      this.tasks.push(this.newTask);
+      this.newTask = getNewTask();
+  }
+  resetPomodoroTimer(){
+      this.currentTime = getPomorodoTime();
+      this.timeLeft = formatTime(this.currentTime);
+  }
+}
+
+function getInitialTasks(){
+    return [
+      { title: 'Write dissertation on ewoks', rank: 0, pomodoros: 3},
+      { title: 'Buy flowers for Malin', rank: 1, pomodoros: 1 }
+    ];
+}
+
+function getPomorodoTime(){
+    return 25*60;
+}
+
+function formatTime(time){
+    const minutesLeft = Number.parseInt(time/60),
+          secondsLeft = Math.round((time/60 - minutesLeft) * 60),
+          formattedMinutesLeft = formatDigits(minutesLeft),
+          formattedSecondsLeft = formatDigits(secondsLeft);
+    return `${formattedMinutesLeft}:${formattedSecondsLeft}`
+
+}
+
+function formatDigits(digits){
+    return digits < 10 ? "0" + digits : digits;
+}
+
+function getNewTask () {
+    return {
+        title: '', 
+        pomodoros: 1,
+        workedPomodoros: 0
+    };
+}
---
+++ 001-monolithic/src/app/main/main.controller.spec.js
@@ -0,0 +1,77 @@
+describe('MainController', () => {
+  let vm,
+      interval;
+
+  beforeEach(angular.mock.module('001Monolithic'));
+
+  beforeEach(inject(($controller, $interval) => {
+    vm = $controller('MainController');
+    interval = $interval;
+  }));
+
+  describe('Tasks', () => {
+
+      it('should define 2 initial tasks', () => {
+        // Arrange, Act, Assert
+        expect(angular.isArray(vm.tasks)).toBeTruthy();
+        expect(vm.tasks.length).toBe(2);
+      });
+
+      it('should be able to add new tasks', () => {
+        // Arrange
+        vm.addNewTask();
+        // Act, Assert
+        expect(vm.tasks.length).toBe(3);
+        const newTask = vm.tasks[2];
+        expect(newTask).toEqual({
+            title: '',
+            pomodoros: 1,
+            workedPomodoros: 0
+        });
+      });
+  });
+
+  describe('Pomodoro', () => {
+
+      it('should start with a timer set to 25:00', () => {
+        // Arrange, Act, Assert
+        expect(vm.timeLeft).toBe("25:00");
+      });
+
+      it('should be able to start the pomodoro', () => {
+        // Arrange, Act
+        vm.startPomodoro();
+        // Assert
+        expect(vm.performingTask).toBe(true);
+      });
+
+      it('should update the pomodoro timer every second', () => {
+        // Arrange, Act
+        vm.startPomodoro();
+        interval.flush(2000);
+        // Assert
+        expect(vm.timeLeft).toBe("24:58");
+      });
+
+      it('should be able to cancel the pomodoro', () => {
+        // Arrange, Act
+        vm.startPomodoro();
+        vm.cancelPomodoro();
+        // Assert
+        expect(vm.performingTask).toBe(false);
+      });
+
+      it('should reset the pomodoro timer after cancelling', () => {
+        // Arrange, Act
+        vm.startPomodoro();
+        interval.flush(2000);
+        vm.cancelPomodoro();
+        // Assert
+        expect(vm.timeLeft).toBe("25:00");
+      });
+
+      afterEach(() => vm.cancelPomodoro());
+
+  });
+
+});
