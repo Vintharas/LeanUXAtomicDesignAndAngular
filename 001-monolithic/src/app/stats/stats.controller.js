c5b63592a938a4ff14abcb5fd3d4520becfd7348
Jaime González García <jaime.gg83@gmail.com>
2016-03-05 18:38:15 +0100

    Added some basic styling to kpis

--- 001-monolithic/src/app/stats/stats.controller.js
+++ 001-monolithic/src/app/stats/stats.controller.js
@@ -14,36 +14,12 @@ export class StatsController{
 
 // this would've been awesome for testing haha
 function getKPIs(tasks){
-    const allEstimatedPomodoros = tasks
-        .map(t => t.pomodoros)
-        .reduce((a,t) => a + t, 0);
-    const allWorkedPomodoros = tasks
-        .map(t => t.workedPomodoros)
-        .reduce((a,t) => a + t, 0);
-    const pomodorosPerDay = tasks
-        .reduce((d,t) => {    
-             if (!d.has(t.timestamp.toDateString())){
-                d.set(t.timestamp.toDateString(), {pomodoros: t.pomodoros, workedPomodoros: t.workedPomodoros});
-             } else {
-                const pomodorosThisDay = d.get(t.timestamp.toDateString());
-                pomodorosThisDay.pomodoros += t.pomodoros;
-                pomodorosThisDay.workedPomodoros += t.workedPomodoros;
-             }
-             return d;
-        }, new Map());
+    const allEstimatedPomodoros = getAllEstimatedPomodoros(tasks);
+    const allWorkedPomodoros = getAllWorkedPomodoros(tasks);
 
-    const totalsPerDay = Array.from(pomodorosPerDay.values())
-        .reduce((a, t) => {
-            return {
-                pomodoros: a.pomodoros + t.pomodoros,
-                workedPomodoros: a.workedPomodoros + t.workedPomodoros
-            };
-        },{pomodoros: 0, workedPomodoros: 0});
-
-    const maxWorkedPomodorosPerDay = Array.from(pomodorosPerDay.values())
-        .reduce((a, t) => {
-            return a > t.workedPomodoros ? a : t.workedPomodoros;
-        }, 0);
+    const pomodorosPerDay = getPomodorosPerDay(tasks);
+    const totalsPerDay = getTotalsPerDay(pomodorosPerDay);
+    const maxWorkedPomodorosPerDay = getMaxWorkedPomodorosPerDay(pomodorosPerDay);
 
     return {
         averageEstimatedPomodoros: allEstimatedPomodoros/tasks.length,
@@ -54,3 +30,47 @@ function getKPIs(tasks){
     }
 
 }
+
+function getAllEstimatedPomodoros(tasks){
+    return tasks
+        .map(t => t.pomodoros)
+        .reduce((a,t) => a + t, 0);
+}
+
+function getAllWorkedPomodoros(tasks){
+    return tasks
+        .map(t => t.workedPomodoros)
+        .reduce((a,t) => a + t, 0);
+}
+
+function getPomodorosPerDay(tasks){
+    return tasks
+        .reduce((d,t) => {    
+             if (!d.has(t.timestamp.toDateString())){
+                d.set(t.timestamp.toDateString(), {pomodoros: t.pomodoros, workedPomodoros: t.workedPomodoros});
+             } else {
+                const pomodorosThisDay = d.get(t.timestamp.toDateString());
+                pomodorosThisDay.pomodoros += t.pomodoros;
+                pomodorosThisDay.workedPomodoros += t.workedPomodoros;
+             }
+             return d;
+        }, new Map());
+}
+
+function getTotalsPerDay(pomodorosPerDay){
+    return Array.from(pomodorosPerDay.values())
+        .reduce((a, t) => {
+            return {
+                pomodoros: a.pomodoros + t.pomodoros,
+                workedPomodoros: a.workedPomodoros + t.workedPomodoros
+            };
+        },{pomodoros: 0, workedPomodoros: 0});
+}
+
+function getMaxWorkedPomodorosPerDay(pomodorosPerDay) {
+    return Array.from(pomodorosPerDay.values())
+        .reduce((a, t) => {
+            return a > t.workedPomodoros ? a : t.workedPomodoros;
+        }, 0);
+}
+
